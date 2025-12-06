60efc7300876f8f2fe76100d90bd5af4a4e56cad
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-12-06 19:24:33 +0530

    Update index.js

--- prathap_profile/src/components/socialicons/index.js
+++ prathap_profile/src/components/socialicons/index.js
@@ -2,22 +2,22 @@ import React from "react";
 import "./style.css";
 import {
   FaGithub,
-  FaTwitter,
   FaFacebookF,
   FaLinkedin,
   FaYoutube,
   FaTwitch,
 } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6"; 
 import { socialprofils } from "../../content_option";
 
-export const Socialicons = (params) => {
+export const Socialicons = () => {
   return (
     <div className="stick_follow_icon">
       <ul>
         {socialprofils.twitter && (
           <li>
             <a href={socialprofils.twitter}>
-              <FaTwitter />
+              <FaXTwitter />  
             </a>
           </li>
         )}
