638bf0711377b4f72b398852893d72f2321f14f2
prathappoojari607 <prathappoojari607@gmail.com>
2026-03-29 23:58:09 +0530

    Delete authConfig.js

--- src/authConfig.js
+++
@@ -1,8 +0,0 @@
-const emailConfig = {
-    YOUR_EMAIL: "prathappoojari607@gmail.com",
-    YOUR_SERVICE_ID: "service_1k44vtb",
-    YOUR_TEMPLATE_ID: "template_kpumzzs",
-    YOUR_USER_ID: "oYMY6D_QwQCk6yeYJ", 
-  };
-
-export { emailConfig };
